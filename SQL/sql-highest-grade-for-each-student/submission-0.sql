96041a17360357e74eadc5efaa0135029a5e7ba7
Mayank Yadav <mayank24343@iiitd.ac.in>
2026-06-27 14:24:49 +0530

    Add: sql-highest-grade-for-each-student - submission-0

---
+++ SQL/sql-highest-grade-for-each-student/submission-0.sql
@@ -0,0 +1,2 @@
+-- Write your query below
+select student_id, min(exam_id) as exam_id, score from exam_results e1 where score = (select max(score) from exam_results e2 where e2.student_id = e1.student_id) group by student_id, score order by student_id;

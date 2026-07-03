ba8cc1dd72917dada93d7afdd95ded896f6692c7
Mayank Yadav <mayank24343@iiitd.ac.in>
2026-07-03 23:01:49 +0530

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int mx = nums[0];
+        int curr = 0;
+        for (int i = 0; i < nums.size(); i++){
+            curr += nums[i];
+            mx = max(curr,mx);
+            if (curr < 0) {
+                curr = 0;
+            }
+        }
+
+        return mx;
+    }
+};
